1655559a66cb76a52eb1d1e245bddd75a812f790
kirill_mur <kiril.murahovskiy@gmail.com>
2025-12-01 14:42:14 +0200

    refactoring - Settings jest config and resolve conflicts
    - the project structure has been slightly changed, the mock package folder has been moved from the projects root directory to the src folder.
    - conflict between tests and Prism has been fixed.
    
    Issue #6

--- api/test/__mocks__/argon2.ts
+++
@@ -1,9 +0,0 @@
-export const hash = jest.fn().mockResolvedValue('hashed-password-global')
-export const verify = jest.fn().mockResolvedValue(true)
-
-const argon2 = {
-	hash,
-	verify
-}
-
-export default argon2
--- api/test/__mocks__/uuid.ts
+++
@@ -1,9 +0,0 @@
-export const v4 = () => 'test-uuid-value'
-export const validate = () => true
-
-const uuid = {
-	v4,
-	validate
-}
-
-export default uuid
