6dc2be5d6fd8aba51d476a7492d40f1060f4fe79
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2025-12-20 04:24:12 +0200

    Update create-chat.dto.ts

--- api/src/irc/chats/dto/create-chat.dto.ts
+++ api/src/irc/chats/dto/create-chat.dto.ts
@@ -1,7 +1,13 @@
 import { IsNotEmpty, IsString } from 'class-validator'
+import { User } from '@prisma/__generated__'
 
 export class CreateChatDto {
 	@IsString()
 	@IsNotEmpty()
 	title: string
+
+	@IsOptional()
+  	@ValidateNested({ each: true })
+  	@Type(() => User)
+  	members: User[];
 }
